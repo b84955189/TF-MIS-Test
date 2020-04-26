b1d0e2be5cc1c1a3723f2c5dbfe8e5c0ea24e69d
Jason Liu <b84955189@vip.qq.com>
2020-04-26 16:52:25 +0800

    IE浏览器下DOM元素无法删除问题（加载页面不更新） bug修复

--- web/js/my.js
+++ web/js/my.js
@@ -46,12 +46,13 @@ function showMsg() {
 
 function loadingFade() {
      var opacity=1;
-     var loadingPage=document.getElementById('loading');
+     //var loadingPage=document.getElementById('loading');
      var loadingBackground=document.getElementById('loading_bg');
      var time=setInterval(function () {
           if (opacity<=0){
                clearInterval(time);
-               loadingPage.remove();
+               //loadingPage.remove();
+               $('#loading').remove();
           }
 
           loadingBackground.style.opacity=opacity;
